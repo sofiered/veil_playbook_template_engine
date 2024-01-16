951cfe44f705159a2a4ac361e891f6e80dd79964
Pavel Tikhachev <p.tihachev@ivideon.com>
2024-01-16 15:21:25 +0400

    add empath constant

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-playbook_names = ('apparatus', 'architect', 'attached', 'catabolist', 'dying')
+playbook_names = ('apparatus', 'architect', 'attached', 'catabolist', 'dying', 'empath')
 no_desciption = 'Описание отсутствует'
