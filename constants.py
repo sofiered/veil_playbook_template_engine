db7692aa79abfbda1ced57c3ac25bc08258247ae
Pavel Tikhachev <p.tihachev@ivideon.com>
2024-01-17 19:33:28 +0400

    add license

--- constants.py
+++ constants.py
@@ -1,2 +1,6 @@
 playbook_names = ('apparatus', 'architect', 'attached', 'catabolist', 'dying', 'empath')
 no_desciption = 'Описание отсутствует'
+original_license = [
+    'Оригинальное произведение "The Veil" авторства  Fraser Simons лицензировано по Creative Commons Share-alike 3.0 Unported (CC BY-SA 3.0).',
+    'Некоторые элементы взяты или адаптированы из Apocalypse World by Vincent Baker с его разрешения.'
+]
